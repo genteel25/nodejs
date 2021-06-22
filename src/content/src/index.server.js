d11fd9e1a81cf18d45ce987de9f0d13ac773eee3
genteel25 <55362732+genteel25@users.noreply.github.com>
2021-06-22 21:19:24 +0100

    index.server.js

--- src/content/src/index.server.js
+++ src/content/src/index.server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     if(req.url === '/about'){
         res.end('Here is our short history')
     }else{
-        res.end(`
+        res.send(`
         <h1>Oops!</h1>
         <p>We can't seem to find the page you are looking for</p>
         <a href="/">Back to home</a>
